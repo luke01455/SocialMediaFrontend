cd94cc753d17124a058799fd352874799ee6cd6e
luke01455 <luke0145555@hotmail.co.uk>
2019-12-30 17:26:40 +0000

    added a few components, pages and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
+import MenuBar from './components/menu-bar'
+
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
 function App() {
   return (
-    <div> 
-      <h1> Social media</h1> 
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
-export default App;
+export default App
