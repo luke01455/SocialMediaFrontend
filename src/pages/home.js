d8890ea3b9f4766228aa85a52e7bd01f4e296cb9
luke01455 <luke0145555@hotmail.co.uk>
2019-12-30 19:10:09 +0000

    added cards from the fetched data from mongodb

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { Grid, Transition } from 'semantic-ui-react'
+
+import PostCard from '../components/postcard'
 
 const Home = () => {
+    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY)
+
     return (
-        <div>
-            <h1> Home Page </h1>
-            </div>
-    );
-};
+      <Grid columns={3}>
+        <Grid.Row>
+          <h1> Recent Posts </h1>
+        </Grid.Row>
+        <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+        </Grid.Row>
+      </Grid>
+    )
+}
 
 
-export default Home;
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`
+
+
+
+export default Home
